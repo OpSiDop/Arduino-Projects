ba5b06d3883cc3ff7e42fc0efe28ea47968442cd
Sidhartha Raghaw <93781577+OpSiDop@users.noreply.github.com>
2022-10-04 18:12:04 +0530

    ino file(code file)

---
+++ Fade/Fade.ino
@@ -0,0 +1,16 @@
+int led = 3;    
+int brightness = 0;   
+int fadeAmount = 5;    
+
+void setup() {
+
+  pinMode(led, OUTPUT);
+}
+void loop() {
+  analogWrite(led, brightness);
+  brightness = brightness + fadeAmount;
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  delay(30);
+}
